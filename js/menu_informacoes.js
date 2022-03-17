dda9fc68a1adb510519eddc582402eaf527bf886
EnzoBraz <enzodesouzabraz2004@gmail.com>
2022-03-17 16:22:14 -0300

    Menu Clientes - Botão Selecionado Consertado
    Co-authored-by: luizfernando103 <luizfernando103@users.noreply.github.com>
    Co-authored-by: Gabriel Guimarães <guimaguima@users.noreply.github.com>

--- js/menu_informacoes.js
+++ js/menu_informacoes.js
@@ -1,23 +1,34 @@
 let botoes = document.querySelectorAll(".btn");
+let paragrafos = document.querySelectorAll(".some");
+let ultimoClickado;
 
 for(botao of botoes){
     botao.addEventListener('click',function(e){
         let id = e.target.value;
         let paragrafo = document.querySelector(`#${id}`);
         paragrafo.style.display = "block";
-        desativaParagrafos(paragrafo);
+        e.target.classList.add("selecionado");
+        desativaAnteriores(paragrafo, e);
     })
 }
 
-function desativaParagrafos(atual){
-    let paragrafos = document.querySelectorAll(".some");
+function desativaAnteriores(paragrafoAtual, e){
+
     for(paragrafo of paragrafos){
-        if(paragrafo.id != atual.id){
+        if(paragrafo.id != paragrafoAtual.id){
             paragrafo.style.display = "none";
             let scroll = document.querySelector('#clientes-descricao');
             scroll.scrollTop = 0;
         }
     }
+    
+    for (botao of botoes) {
+        if(botao != e.target){
+            botao.classList.remove("selecionado");
+        }
+    }
+
+
 }
 
 
@@ -26,4 +37,4 @@ let eventoDeClick = document.createEvent('MouseEvents');
 eventoDeClick.initEvent('click',true,true);
 botaoInicial.dispatchEvent(eventoDeClick);
 
-document.getElementById("primeiro").focus();
+document.getElementById("primeiro").classList.add("selecionado");
